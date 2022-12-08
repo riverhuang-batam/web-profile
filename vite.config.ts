7d47e4248c06fc69b601a82406df5d6ff8c5d21d
“riverhuang-batam” <riverhuang011@gmail.com>
2022-12-08 21:16:44 +0700

    fix images issue

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,15 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue({
+    // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
+    // For example <img src="/images/logo.png"> will not work without the code below
+    template: {
+        transformAssetUrls: {
+            includeAbsolute: false,
+        },
+    },
+}), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
